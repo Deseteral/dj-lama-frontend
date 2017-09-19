265a749f61bad388f33528789f3abb1ba5f2f097
Deseteral <deseteral@gmail.com>
2017-09-19 15:42:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DJ Lama
+
+User driven radio station - React frontend app.
+
+For corresponding backend service look at [dj-lama](https://github.com/Deseteral/dj-lama).
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
+// registerServiceWorker();
