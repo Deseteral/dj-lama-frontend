e90668d5d380a13ed2f000668d4ee30c810250c3
Deseteral <deseteral@gmail.com>
2018-08-11 10:46:10 +0200

    Fixed tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
